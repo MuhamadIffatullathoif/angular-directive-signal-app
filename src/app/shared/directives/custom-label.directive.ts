21786caa4d42784013e5dfa1a06e97f676bc1086
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-26 08:59:58 +0700

    03. Custom Label Custom Directive

---
+++ src/app/shared/directives/custom-label.directive.ts
@@ -0,0 +1,12 @@
+import {Directive, ElementRef} from '@angular/core';
+
+@Directive({
+  selector: '[customLabel]'
+})
+export class CustomLabelDirective {
+  private htmlElement?: ElementRef<HTMLElement>;
+  constructor(private el: ElementRef<HTMLElement>) {
+    this.htmlElement = el;
+  }
+
+}
