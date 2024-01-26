ed5e639f5025374c6bc82fb311a2ab5542bd8f81
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-26 09:45:17 +0700

    05. Change The Label Message

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,14 +1,22 @@
 import {Directive, ElementRef, Input, OnInit} from '@angular/core';
+import {ValidationErrors} from "@angular/forms";
 
 @Directive({
   selector: '[customLabel]'
 })
-export class CustomLabelDirective implements OnInit{
+export class CustomLabelDirective implements OnInit {
   private htmlElement?: ElementRef<HTMLElement>;
   private _color: string = 'red';
+  private _errors?: ValidationErrors | null;
+
   @Input() set color(value: string) {
     this._color = value;
     this.setStyle();
+  }
+
+  @Input() set error(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    this.setErrorMessage();
   }
 
   constructor(private el: ElementRef<HTMLElement>) {
@@ -24,4 +32,29 @@ export class CustomLabelDirective implements OnInit{
     this.htmlElement.nativeElement.style.color = this._color;
   }
 
+  setErrorMessage(): void {
+    if (!this.htmlElement) return;
+    if (!this._errors) {
+      this.htmlElement.nativeElement.innerHTML = '';
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+    if (errors.includes('required')) {
+      this.htmlElement.nativeElement.innerHTML = 'This field is required';
+      return;
+    }
+
+    if(errors.includes('minlength')) {
+      const min = this._errors['minlength']['requiredLength'];
+      const current = this._errors['minlength']['actualLength'];
+
+      this.htmlElement.nativeElement.innerHTML = `Minimum ${current}/${min} characters`;
+      return;
+    }
+
+    if(errors.includes('email')) {
+      this.htmlElement.nativeElement.innerHTML = 'Does not have correct format';
+    }
+  }
 }
